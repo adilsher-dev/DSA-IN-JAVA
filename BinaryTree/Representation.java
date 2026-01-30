d8cc4bb89ba999bf72162b92a1666140ecded41a
ADIL SHER <saajidali2789@gmail.com>
2026-01-30 19:40:22 +0530

    Nothing

--- BinaryTree/Representation.java
+++ BinaryTree/Representation.java
@@ -15,7 +15,7 @@ class Node{
 public class Representation {
     public static void main(String[] args) {
         Node root = new Node(1);
-        root.left = new Node(2);
+        root.left = new Node(4);
         root.right = new Node(3);
         root.right.left = new Node(5);
     }
